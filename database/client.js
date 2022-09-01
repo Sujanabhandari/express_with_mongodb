5c1a4aa3755d360afd97b44ffa147a2b3f6eb63b
Sujana Bhandari <sujana6589@gmail.com>
2022-09-01 13:18:06 +0200

    Bootstrap express project with mongodb

---
+++ database/client.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const { DATABASE_CONNECTION_URL } = process.env;
+
+module.exports = mongoose.connect(
+    DATABASE_CONNECTION_URL)
+    .then(() => console.log("Connected Successfully"))
+    
+  .catch(error => error);
